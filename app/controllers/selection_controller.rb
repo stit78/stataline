2453d7831aa38b786cf286158fe5071514873b3f
thibaut salle <thibaut.m.salle@gmail.com>
2019-03-08 17:14:20 +0100

    begin controller

--- app/controllers/selection_controller.rb
+++ app/controllers/selection_controller.rb
@@ -4,5 +4,17 @@ class SelectionController < ApplicationController
     @subtopic = Subtopic.all
     @countries = Country.all
     @indicators = Indicator.all
+    @new_request = ""
+  end
+
+  def create
+    @element = Element.where("country_id = ? AND indicator_id = ?", create_params[:country].to_i, create_params[:indicator].to_i).first
+    @element_clear = @element.series.reject { |year, value| value == "-" }
+  end
+
+  private
+
+  def create_params
+    params.permit(:topic, :country, :subtopic, :indicator)
   end
 end
--- app/models/element.rb
+++ app/models/element.rb
@@ -2,4 +2,8 @@ class Element < ApplicationRecord
   belongs_to :country
   belongs_to :indicator
   belongs_to :unit
+
+  def hello
+    return true
+  end
 end
